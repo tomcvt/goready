0c0ae85245d4ec766592bafa0c4ffe5152daa973
tomcvt <tomcvtdev@gmail.com>
2026-01-06 04:39:54 +0100

    Initial Commit

---
+++ app/src/main/java/com/tomcvt/goready/domain/alarm/AlarmManager.kt
@@ -0,0 +1,10 @@
+package com.tomcvt.goready.domain.alarm
+
+class AlarmManager {
+
+
+
+    fun createAlarm(time: String, label: String, repeat: Boolean) {
+        // Implementation to create an alarm
+    }
+}
